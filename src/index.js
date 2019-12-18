75be55b848f462fe499d8c8544ee7b09b0c07831
mapengbo0314 <mapengbo0314@gmail.com>
2019-12-17 23:31:43 -0600

    added some starting function and tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const todo = () => {
-    console.log("prints");
-}
+const todo = (title, description) => {
+  const output = () => {
+    return `${title} is listed to be done for the reasons of ${description}`;
+  };
 
-todo();
+  return { output };
+};
+
+const task1 = todo("Go sleep", "Cuz u got to go to work tommorow");
+
+let htmlfinder = document.getElementById("title");
+let task1input = document.createElement("p");
+task1input.textContent = task1.output();
+
+htmlfinder.appendChild(task1input);
